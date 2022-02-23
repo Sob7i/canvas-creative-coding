b0263079ae1b6437a7bb8967d0925a37fd3c6dfc
Sobhi <sub7i.alkhuder@gmail.com>
2022-02-23 23:54:46 +0100

    Write comments

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # What is this repo about?
-A list of creative coding projects using HTML canvas element
+A list of creative coding projects using HTML canvas element.
   
 # How to run locally?
 The easiest way is to use [live server](https://github.com/ritwickdey/vscode-live-server) 
--- particles-network/script.js
+++ particles-network/script.js
@@ -3,37 +3,44 @@ const canvas = document.getElementById('canvas')
 /** @type {CanvasRenderingContext2D} */
 const ctx = canvas.getContext('2d')
 const particlesArray = []
-let rgb = 0
 
-// Canvas base setup 
+// When document is loaded, create particles pattern
 window.addEventListener('load', () => {
+  // set canvas size to window size
   canvas.width = window.innerWidth
   canvas.height = window.innerHeight
+
+  // set canvas background to black
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  // populate particlesArray with particles
+  // populate particlesArray with particles.
   for (let i = 0; i <= canvas.width / 4; i++) {
     particlesArray.push(new Particles())
   }
-  // Create a pattern of particles
+  // Create a pattern of particles.
   createPattern()
 })
 
-// Maintain canvas width and height when widow's size changes
+// When document loose focus, reacreate the pattern.
 window.addEventListener('onblur', () => {
+  // Set the canvas background to black.
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
+  // Reacrate the pattern.
   createPattern()
 })
 
-// Maintain canvas width and height when widow's size changes
 window.addEventListener('resize', () => {
+  // Maintain canvas width and height when widow's size changes.
   canvas.width = window.innerWidth
   canvas.height = window.innerHeight
+  // Set the canvas background to black.
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+  // Set array of particles to empty,
+  // then populate particlesArray with particles again
   particlesArray.length = 0
   for (let i = 0; i <= canvas.width / 4; i++) {
     particlesArray.push(new Particles())
@@ -51,13 +58,16 @@ class Particles {
   }
 
   draw() {
+    // Draw the particle.
     ctx.beginPath()
     ctx.fillStyle = 'white'
     ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2)
     ctx.fill()
+    ctx.closePath()
   }
 
   move() {
+    // Move the particle by adding speed to its position.
     this.x += this.speedX / 4
     this.y += this.speedY / 4
 
@@ -65,7 +75,6 @@ class Particles {
     if (this.x > canvas.width || this.x < 0) {
       this.speedX = -this.speedX
     }
-
     if (this.y > canvas.height || this.y < 0) {
       this.speedY = -this.speedY
     }
@@ -99,7 +108,7 @@ function createPattern() {
 
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
-  ctx.fillStyle = "rgba(0, 0, 0, 1 )";
+  ctx.fillStyle = "rgba(0, 0, 0, 1)";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   createPattern()
   requestAnimationFrame(animate)
