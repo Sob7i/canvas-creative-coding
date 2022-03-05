f57d4019237d79ed336d113eea5387ad22793f56
Sobhi Al Khuder <sub7i.alkhuder@gmail.com>
2022-03-05 01:18:28 +0100

    part 1 - camera

--- src/kaleidoscope-p5/sketch.js
+++ src/kaleidoscope-p5/sketch.js
@@ -1,61 +1,112 @@
-function setup() {
-  createCanvas(window.innerWidth, window.innerHeight);
-}
-
-let tSide = 150;
+let tSide = 450;
 let tHeight = (tSide * Math.sqrt(3)) / 2;
 
-function draw() {
-  background(220);
-  translate(width / 2 - tSide / 2, height / 2 - tHeight / 2);
-
-  // * Draw a Hexagon 
-  // // Top middle
-  // triangle(tSide, 0, tSide / 2, tHeight, 0, 0);
-  // // Top left
-  // triangle(0, 0, -tSide / 2, tHeight, tSide / 2, tHeight);
-  // // Top right
-  // triangle(tSide, 0, tSide + tSide / 2, tHeight, tSide / 2, tHeight);
-
-  // // Bottom right
-  // triangle(tSide + tSide / 2, tHeight, tSide, 2* tHeight, tSide / 2, tHeight);
-  // // Bottom left
-  // triangle(tSide / 2, tHeight, 0, 2 * tHeight, -tSide / 2, tHeight);
-  // // Bottom middle
-  // triangle(tSide / 2, tHeight, 0, 2 * tHeight, tSide, 2 * tHeight);
-
-
-  // Fill up the screen with triangles programmatically
-  for (let i = 0; i < width; i += tSide) {
-    for (let j = 0; j < height; j += tHeight) {
-      // Top middle
-      triangle(i, j, i + tSide / 2, j + tHeight, i, j + tHeight);
-      // Top left
-      triangle(i, j, i - tSide / 2, j + tHeight, i + tSide / 2, j + tHeight);
-      // Top right
-      triangle(i + tSide, j, i + tSide / 2, j + tHeight, i + tSide / 2, j + tHeight);
-
-      // Bottom right
-      triangle(i + tSide / 2, j + tHeight, i + tSide, j + 2 * tHeight, i + tSide / 2, j + tHeight);
-      // Bottom left
-      triangle(i + tSide / 2, j + tHeight, i, j + 2 * tHeight, i - tSide / 2, j + tHeight);
-      // Bottom middle
-      triangle(i + tSide / 2, j + tHeight, i, j + 2 * tHeight, i, j + 2 * tHeight);
-    }
-  }
-
-}
-
-function mouseWheel(event) {
-  if (event.delta > 0) {
-    tSide += 30;
-    tHeight = (tSide * Math.sqrt(3)) / 2;
-  } else {
-    tSide -= 30;
-    tHeight = (tSide * Math.sqrt(3)) / 2;
-  }
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+  // noLoop();
 }
 
 function windowResized() {
   resizeCanvas(window.innerWidth, window.innerHeight);
 }
+
+function mouseWheel(event) {
+  if (event.delta > 0) {
+    if (tSide >= 700) return;
+    tSide += 30;
+    tHeight = (tSide * Math.sqrt(3)) / 2;
+  } else {
+    if (tSide <= 70) return;
+    tSide -= 30;
+    tHeight = (tSide * Math.sqrt(3)) / 2;
+  }
+}
+
+function draw() {
+  background("black");
+  // translate(width / 2 - tSide / 2, height / 2 - tHeight / 2);
+  createPattern();
+}
+
+function createPattern() {
+  let isOddRow = false;
+  let xOffset = 0;
+  let yOffset = 0;
+
+  for (let x = 0; x < width; x += tSide) {
+    if (isOddRow) {
+      xOffset = tSide / 2;
+      yOffset = tHeight;
+    } else {
+      xOffset = 0;
+      yOffset = 0;
+    }
+
+    for (let y = 0; y < height; y += tHeight) {
+      // Draw the triangle.
+      // triangle(i, y, i + tSide / 2, y + tHeight, i + tSide, y);
+
+      if (isOddRow) triangle(x, y, x + tSide / 2, y + tHeight, x + tSide, y);
+      else triangle(x, y, x + tSide / 2, y + tHeight, x + tSide, y);
+    }
+
+    isOddRow = !isOddRow;
+  }
+}
+
+// ellipse(i + tSide / 2, tHeight, 50, 50);
+// triangle(
+//   i + tSide / 2,
+//   j + tHeight,
+//   i + tSide,
+//   j ,
+//   i + tSide + tSide / 2,
+//   j + tHeight
+// );
+
+// Draw a row of triangles
+// for (let i = 0; i < width; i += tSide) {
+//   // Up middle triangle both directions
+//   triangle(i, 0, i + tSide / 2, tHeight, i + tSide, 0);
+//   triangle(-i, 0, -i + tSide / 2, tHeight, -i + tSide, 0);
+
+//   // Up right and left triangles
+//   triangle(
+//     i + tSide / 2,
+//     tHeight,
+//     i + tSide,
+//     0,
+//     i + tSide + tSide / 2,
+//     tHeight
+//   );
+//   triangle(
+//     -i + tSide / 2,
+//     tHeight,
+//     -i + tSide,
+//     0,
+//     -i + tSide + tSide / 2,
+//     tHeight
+//   );
+
+//   // Down middle triangle both directions
+//   triangle(i + tSide / 2, tHeight, i + tSide, 2 * tHeight, i, 2 * tHeight);
+//   triangle(-i + tSide / 2, tHeight, -i + tSide, 2 * tHeight, -i, 2 * tHeight);
+
+//   // Down right and left triangles
+//   triangle(
+//     i + tSide + tSide / 2,
+//     tHeight,
+//     i + tSide,
+//     2 * tHeight,
+//     i + tSide / 2,
+//     tHeight
+//   );
+//   triangle(
+//     -i + tSide + tSide / 2,
+//     tHeight,
+//     -i + tSide,
+//     2 * tHeight,
+//     -i + tSide / 2,
+//     tHeight
+//   );
+// }
