87be4db59a5a4b5e44af6295acd93afe4847f06c
Sobhi Al Khuder <sub7i.alkhuder@gmail.com>
2022-07-16 17:00:43 +0200

    add second sequence

--- src/images-sequence/pages/index.tsx
+++ src/images-sequence/pages/index.tsx
@@ -1,18 +1,26 @@
 import { useEffect, useState } from "react";
 import ImgSequenceScroll from "../hooks/useScrollSequence";
 
-const prioFrames = [0, 25, 50, 75, 100, 125]
+// Apple airpod sequence -> 131 frames
 const BASE_URL = 'https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/02-head-bob-turn/'
+const prioFrames = [0, 25, 50, 75, 100, 125]
+const framesCount = 131
+const padStart = 4
+
+// In the forest sequence -> 98 frames
+// const BASE_URL = 'https://jacobbelanger.com/assets/sequence/'
+// const prioFrames = [0, 20, 40, 60, 80, 90, 98]
+// const framesCount = 98
+// const padStart = 3
 
 export default function Home() {
   const [frames, setFrames] = useState([])
-  const [framesCount, setFramesCount] = useState(131)
 
   useEffect(() => {
     let appleSequenceImages = []
 
     for (let i = 0; i <= framesCount; i++) {
-      const num = i.toString().padStart(4, '0')
+      const num = i.toString().padStart(padStart, '0')
       const format = '.jpg'
 
       const src = `${BASE_URL}${num}${format}`
@@ -20,7 +28,7 @@ export default function Home() {
     }
 
     setFrames(appleSequenceImages)
-  }, [framesCount])
+  }, [])
 
   return (
     <>
